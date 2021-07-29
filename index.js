7cbb042f30135694266e782540590fc45685c89f
Pawan Kumar (bellrd) <pawankumar2399@gmail.com>
2021-07-29 18:28:18 +0530

    added failsafe methods

--- index.js
+++ index.js
@@ -3,10 +3,29 @@ const multer = require("multer");
 
 const app = express();
 
-const upload = multer({ dest: "uploads/" });
+let storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, "uploads/");
+  },
+  filename: function (req, file, callback) {
+    callback(null, file.originalname);
+  },
+});
 
-app.post("/upload", upload.single('file_name'), async (req, res) => {
-  return res.status(200).json({ msg: "success" });
+const upload = multer({ storage });
+
+app.post("/upload", upload.single("file_name"), async (req, res) => {
+  try {
+    console.log(req.body);
+    let pwd = req.body.pwd;
+    if (pwd != "ckret2020") {
+      return res.status(400).json({ msg: "failed" });
+    }
+    return res.status(200).json({ msg: "success" });
+  } catch (e) {
+    console.log({ e });
+    return res.status(400).json({ msg: "failed" });
+  }
 });
 
 app.listen(9000, () => console.log("server started"));
